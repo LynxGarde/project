74648e6affe65ae0bc857a26aae7b37c23e5fe45
Leo Tordjman <leo.tordjman@epita.fr>
2018-11-16 22:23:45 +0100

    [MERGE][TESTS] ++tests

---
+++ tests/test_O.py
@@ -0,0 +1,36 @@
+import subprocess
+import os
+
+def test_minus_O():
+    out = open("out.tst", "w+")
+    err = open("err.tst", "w+")
+    subprocess.call(["../build/42sh", "-O"], stdout=out, stderr=err)
+    out.close()
+    err.close()
+    out = open("out.tst", "r")
+    err = open("err.tst", "r")
+    str_out = out.read()
+    str_err = err.read()
+    out.close()
+    err.close()
+    os.remove("out.tst")
+    os.remove("err.tst")
+    assert(str_out == "-O or +O\n")
+    assert(str_err == "")
+
+def test_plus_O():
+    out = open("out.tst", "w+")
+    err = open("err.tst", "w+")
+    subprocess.call(["../build/42sh", "+O"], stdout=out, stderr=err)
+    out.close()
+    err.close()
+    out = open("out.tst", "r")
+    err = open("err.tst", "r")
+    str_out = out.read()
+    str_err = err.read()
+    out.close()
+    err.close()
+    os.remove("out.tst")
+    os.remove("err.tst")
+    assert(str_out == "-O or +O\n")
+    assert(str_err == "")
