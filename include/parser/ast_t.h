eeda674583d2f3d58fde1885dbe3b0b28d993efa
Leo Tordjman <leo.tordjman@epita.fr>
2018-11-16 07:45:14 +0100

    [AST][AST_T] +-struct ast_t

--- include/parser/ast_t.h
+++ include/parser/ast_t.h
@@ -8,30 +8,43 @@
 #include "list.h"
 
 /*!
+ * \struct key
+ * \brief Struct key
+ */
+struct key
+{
+    enum token t;
+    char *str;
+};
+
+/*!
  * \struct ast_t
  * \brief Struct ast_t
  */
 struct ast_t
 {
-    char *key;
-    struct ast_t *child;
-    struct ast_t *sibling;
+    struct key key;
+    struct ast_t *cond;
+    struct ast_t *if_true;
+    struct ast_t *if_false;
 };
 
 /*!
- * \fn struct ast_t *ast_init(char *key, struct ast_t *child,
-    struct ast_t *sibling)
+ * \fn struct ast_t *ast_init(struct key key,
+    struct ast_t *cond, struct ast_t *if_true, struct ast_t *if_false)
 
  * \brief Initialize a new instance of the struct ast_t and return a pointer.
 
  * \param key: value of current node.
- * \param child: first child.
- * \param sibling: first sibling.
+ * \param cond: node condition.
+ * \param if_true: node if condition returns true.
+ * \param if_false: node if condition returns false.
 
  * \return Returns a new pointer on a new instance of struct ast_t. On error,
     returns NULL.
  */
-struct ast_t *ast_init(char *key, struct ast_t *child, struct ast_t *sibling);
+struct ast_t *ast_init(struct key key,
+    struct ast_t *cond, struct ast_t *if_true, struct ast_t *if_false);
 
 /*!
  * \fn void ast_delete(struct ast_t *ast)
@@ -45,12 +58,13 @@ struct ast_t *ast_init(char *key, struct ast_t *child, struct ast_t *sibling);
 void ast_delete(struct ast_t *ast);
 
 /*!
- * \fn struct ast_t *ast_build(struct list *l)
+ * \fn struct ast_t *ast_build(struct ast_t *ast, struct list *l)
  * \brief Build an AST from the given list.
 
+ * \param ast: The AST to build;
  * \param list: The list from witch the AST will be built.
 
  * \return Returns a new pointer on a new instance of struct ast_t. On error,
     returns NULL.
  */
-struct ast_t *ast_build(struct list *l);
+struct ast_t *ast_build(struct ast_t *ast, struct list *l);
--- src/parser/ast_t.c
+++ src/parser/ast_t.c
@@ -2,14 +2,16 @@
 
 #include "ast_t.h"
 
-struct ast_t *ast_init(char *key, struct ast_t *child, struct ast_t *sibling)
+struct ast_t *ast_init(struct key key,
+    struct ast_t *cond, struct ast_t *if_true, struct ast_t *if_false)
 {
     struct ast_t *ast = malloc(sizeof(struct ast_t));
     if (!ast)
         return NULL;
     ast->key = key;
-    ast->child = child;
-    ast->sibling = sibling;
+    ast->cond = cond;
+    ast->if_true = if_true;
+    ast->if_false = if_false;
     return ast;
 }
 
@@ -17,10 +19,31 @@ void ast_delete(struct ast_t *ast)
 {
     if (ast)
     {
-        if (ast->child)
-            ast_delete(ast->child);
-        if (ast->sibling)
-            ast_delete(ast->sibling);
+        if (ast->cond)
+            ast_delete(ast->cond);
+        if (ast->if_true)
+            ast_delete(ast->if_true);
+        if (ast->if_false)
+            ast_delete(ast->if_false);
         free(ast);
     }
+}
+
+struct ast_t *ast_build(struct ast_t *ast, struct list *l)
+{
+    if (!l)
+        return NULL;
+    if (!l->head)
+    {
+        list_destroy(l); //This list cannot be reused.
+        return ast;
+    }
+    struct list_elt *elt = list_pop(l);
+    struct key key;
+    key.t = elt->t;
+    key.str = elt->strtoken;
+    free(elt);
+    if (key.t == IF)
+        return NULL;
+    return NULL;
 }
